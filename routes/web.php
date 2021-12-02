cb16ced1532b26e76ec7416ab005eac84dcb44db
agungh21 <agungmuhammad70@gmail.com>
2021-12-02 09:00:14 +0700

    setting routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/sekolah-about', function () {
+    return view('about');
+});
+
+Route::get('/sekolah-blog', function () {
+    return view('blog');
 });
