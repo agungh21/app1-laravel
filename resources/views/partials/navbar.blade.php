7b84fd2d4cfb750a993975348774b05e078c56e0
agungh21 <agungmuhammad70@gmail.com>
2021-12-02 09:58:05 +0700

    navbar active dengan operasi trinary

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,20 +1,20 @@
 {{-- navbar --}}
 <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
     <div class="container">
-      <a class="navbar-brand" href="#">Sekolah</a>
+      <a class="navbar-brand" href="/">Sekolah</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" href="/">Home</a>
+            <a class="nav-link {{ ($title === "home") ? 'active' : '' }}" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/sekolah-about">About</a>
+            <a class="nav-link {{ ($title === "about") ? 'active' : '' }}" href="/sekolah-about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/sekolah-blog">Blog</a>
+            <a class="nav-link {{ ($title === "blog") ? 'active' : '' }}" href="/sekolah-blog">Blog</a>
           </li>
         </ul>
       </div>
